578f6802cbbab2919003d3fb0dc507b02abb615e
Bradley Dufour <dufour.bradley@gmail.com>
2017-04-14 17:41:12 -0400

    Adds EventFactory and comments all necessary methods

---
+++ EventFactory.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/** EventFactory deals with the execution of any Item induced events.
+ * with an object.
+ * @author Team Red
+ *
+ */
+import java.util.ArrayList;
+public class EventFactory {
+	
+	/** Executes a list of Strings representing events that are related to the parameter Item
+	 * 
+	 * @param item Item which the parameter events are related to
+	 * @param events Set of Strings representing events to execute, potentially on the parameter Item
+	 */
+	public static void execute(Item item, ArrayList<String> events)
+	{
+		for(String event:events)
+		{
+			
+		}
+	}
+}
