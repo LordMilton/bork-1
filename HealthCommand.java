997b7c96a6e3563e1efaed94b4a6e65ca97fa4c0
Bradley Dufour <dufour.bradley@gmail.com>
2017-04-14 12:38:43 -0400

    Implements HealthCommand.java's execute() method

--- HealthCommand.java
+++ HealthCommand.java
@@ -20,6 +20,18 @@ class HealthCommand extends Command{
 	 */
 	String execute()
 	{
+		GameState state = GameState.instance();
+		int currentHealth = state.getAdventurersHealth();
 		
+		if(currentHealth >= 41)
+			return "You feel as healthy and strong as ever";
+		else if(currentHealth >= 31)
+			return "Your wounds sting slightly, but you have plenty of strength for adventuring";
+		else if(currentHealth >= 21)
+			return "Your health has clearly degraded";
+		else if(currentHealth >= 11)
+			return "You stagger from the pain of your wounds";
+		else
+			return "You feel as if you have one foot in your grave already";
 	}
 }
