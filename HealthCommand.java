bd5ba33fb2c34fba3e3542c90a312b74f9e0ddd4
DanielPSchaub <DanielP.Schaub@gmail.com>
2017-04-14 22:44:03 -0400

    Adding functionality to HealthCommand and ScoreCommand

--- HealthCommand.java
+++ HealthCommand.java
@@ -1,25 +1,43 @@
+
 /**
- * 
+ *
  */
 
-/** HealthCommand deals with any Commands relating to a player checking their health.
+/**
+ * HealthCommand deals with any Commands relating to a player checking their health.
+ *
  * @author Team Red
  *
  */
-class HealthCommand extends Command{
-	
-	/** Instantiates a HealthCommand
-	 * 
-	 */
-	HealthCommand()
-	{
-	}
-	
-	/**
-	 * @override From Command
-	 */
-	String execute()
-	{
-		
-	}
+class HealthCommand extends Command {
+
+    private int health;
+
+    /**
+     * Instantiates a HealthCommand
+     *
+     */
+    HealthCommand() {
+        this.health = GameState.instance().getAdventurersHealth();
+    }
+
+    /**
+     * @override From Command Made up some bogus health values, 1-3
+     */
+    String execute() {
+        switch (health) {
+            case 1:
+                return "You are about to die.";
+            case 2:
+                return "Each step is a stagger"
+                        + "from the pain of your wounds.";
+            case 3:
+                return "You're a bit light-headed.";
+            case 4:
+                return "You are about to die.";
+            default:
+                return "You feel fit as a fiddle.";
+        }
+
+    }
 }
--- ScoreCommand.java
+++ ScoreCommand.java
@@ -1,26 +1,44 @@
-/**
- * 
- */
 
-/** ScoreCommand deals with any Commands relating to a player checking their score.
+/**
+ *
+ */
+/**
+ * ScoreCommand deals with any Commands relating to a player checking their score.
+ *
  * @author Team Red
  *
  */
- 
-class ScoreCommand extends Command{
+class ScoreCommand extends Command {
 
-	/** Instantiates a ScoreCommand
-	 * 
-	 */
-	ScoreCommand()
-	{
-	}
-	
-	/**
-	 * @override From Command
-	 */
-	String execute()
-	{
-		
-	}
+    private int score;
+    private String rank;
+
+    /**
+     * Instantiates a ScoreCommand
+     *
+     */
+    ScoreCommand() {
+        this.score = GameState.instance().getAdventurersScore();
+
+        if (score <= 10) {
+            rank = "Amateur Adventurer";
+        }
+        if (score > 10 && score <= 50) {
+            rank = "Novice Adventurer";
+        }
+        if (score > 50 && score <= 100) {
+            rank = "Proficient Adventurer";
+        }
+        if (score > 100) {
+            rank = "Legendary Adventurer";
+        }
+    }
+
+    /**
+     * @override From Command
+     */
+    String execute() {
+        return "You have accumulated" + score + " points."
+                + "This gives you a rank of " + this.rank + ".";
+    }
 }
