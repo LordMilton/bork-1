de794faec9dc670b46c52cd259a08977166882fc
DanielPSchaub <DanielP.Schaub@gmail.com>
2017-04-14 23:20:43 -0400

    Adds rank to text output from ScoreCommand
    Git has been giving me a headache for the past 3 hours so I'm just going to manually add this here and silently weep to myself for writing code that's already been written.

--- ScoreCommand.java
+++ ScoreCommand.java
@@ -9,11 +9,29 @@
  
 class ScoreCommand extends Command{
 
+	
+	    private int score;
+	    private String rank;
+
 	/** Instantiates a ScoreCommand
 	 * 
 	 */
 	ScoreCommand()
-	{
+	{	
+		this.score = GameState.instance().getAdventurersScore();
+
+		if (score <= 10) {
+		    rank = "Amateur Adventurer";
+		}
+		if (score > 10 && score <= 50) {
+		    rank = "Novice Adventurer";
+		}
+		if (score > 50 && score <= 100) {
+		    rank = "Proficient Adventurer";
+		}
+		if (score > 100) {
+		    rank = "Legendary Adventurer";
+		}
 	}
 	
 	/**
@@ -21,9 +39,12 @@ class ScoreCommand extends Command{
 	 */
 	String execute()
 	{
-		GameState state = GameState.instance();
-		int currScore = state.getAdventurersScore();
+		/** Edited these out for easy replacement in case of bad things happening **/
+		//GameState state = GameState.instance();
+		//int currScore = state.getAdventurersScore();
 		
-		return("Your score is "+ currScore);
+		//return("Your score is "+ currScore);
+		return "You have accumulated" + score + " points."
+                + "This gives you a rank of " + this.rank + ".";
 	}
 }
