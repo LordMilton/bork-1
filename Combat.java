abcf0e3972a5b9c159b30df320977032890d73bf
Bradley Dufour <dufour.bradley@gmail.com>
2017-04-21 18:34:18 -0400

    Adjusts Combat's attack() for AttackCommand's expectations

--- Combat.java
+++ Combat.java
@@ -11,10 +11,13 @@ class Combat {
 	
 	/** Causes an attack upon the specified NPC (or the player) with the specified Item
 	 * 
-	 * @param weapon Item being used to attack victim
+	 * @param weapon Item being used to attack victim, not checked if the Item is actually defined as a weapon (this
+	 * should be checked before calling attack)
 	 * @param victim NPC being attacked, if null then the player is being attacked
+	 * @return A message stating whether or not the parameter victim was hit and with the proper person depending if
+	 * the parameter victim is an NPC or the player
 	 */
-	public static void attack(Item weapon, NPC victim)
+	public static String attack(Item weapon, NPC victim)
 	{
 		
 	}
