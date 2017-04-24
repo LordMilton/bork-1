d8a60c0efaa05bc5483da0120647befa9ba1d739
Bradley Dufour <dufour.bradley@gmail.com>
2017-04-24 13:13:59 -0400

    Changed method name for clarification

--- GameState.java
+++ GameState.java
@@ -220,7 +220,7 @@ public class GameState {
      * @return The NPC with the parameter name as its name, if one is in the adventurersCurrentRoom
      * @throws NPC.NoNPCException If there is no NPC going by the parameter name in the adventurersCurrentRoom
      */
-    NPC getNPCFromVicinityNamed(String name) throws NPC.NoNPCException
+    NPC getNPCInVicinityNamed(String name) throws NPC.NoNPCException
     {
     	if(dungeon.getNPC(name).getCurrentRoom() == adventurersCurrentRoom)
     	{
