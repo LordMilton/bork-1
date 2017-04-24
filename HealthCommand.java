e3e09ffd9c60b698f7b56d40b801abb847c7efce
DanielPSchaub <DanielP.Schaub@gmail.com>
2017-04-23 22:00:39 -0400

    "Setting up fresh repository"

---
+++ HealthCommand.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/** HealthCommand deals with any Commands relating to a player checking their health.
+ * @author Team Red
+ *
+ */
+class HealthCommand extends Command{
+	
+	/** Instantiates a HealthCommand
+	 * 
+	 */
+	HealthCommand()
+	{
+	}
+	
+	/**
+	 * @override From Command
+	 */
+	String execute()
+	{
+		GameState state = GameState.instance();
+		int currentHealth = state.getAdventurersHealth();
+		
+		if(currentHealth >= 41)
+			return "You feel as healthy and strong as ever\n";
+		else if(currentHealth >= 31)
+			return "Your wounds sting slightly, but you have plenty of strength for adventuring\n";
+		else if(currentHealth >= 21)
+			return "Your health has clearly degraded\n";
+		else if(currentHealth >= 11)
+			return "You stagger from the pain of your wounds\n";
+		else
+			return "You feel as if you have one foot in your grave already\n";
+	}
+}
